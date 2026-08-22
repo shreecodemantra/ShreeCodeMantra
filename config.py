5dcfc692d9c7d1c985452bab5f0463c78cf06ede
Roshan Mundekar <rmundekar2000@gmail.com>
2026-08-22 14:04:22 +0530

    Add complete project code

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/shreecodemantra")
-    SECRET_KEY = os.getenv("SECRET_KEY", "Shree@1122")
+    MONGO_URI = os.getenv("MONGO_URI")
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
